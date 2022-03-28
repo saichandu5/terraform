ecba9c36d1eb8346d0674e0ec541b89d2d76fbcd
saichandu5 <94437978+saichandu5@users.noreply.github.com>
2022-03-28 08:31:55 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,33 @@
+# Security Group
+resource "aws_security_group" "hdfc-web" {
+  name        = "hdfc-web"
+  description = "Allow SSH - HTTP inbound traffic"
+  vpc_id      = aws_vpc.hdfc.id
+
+  ingress {
+    description = "SSH from WWW"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from WWW"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "HDFC-WEB"
+  }
+}
